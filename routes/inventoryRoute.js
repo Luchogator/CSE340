115bab992321e852defbf6cbe9c3eaa5c05a8f15
Luis Lizano <119129151+Luchogator@users.noreply.github.com>
2025-06-07 23:50:36 -0500

    fix navigation styling and vehicle images

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = new express.Router();
 const invController = require("../controllers/invController");
 
+// Route to build inventory by classification view
+router.get("/type/:classificationId", invController.buildClassificationView);
+
 // Route to build inventory detail view
-// Example: /inv/detail/1
 router.get("/detail/:inv_id", invController.buildVehicleDetailPage);
 
 module.exports = router;
